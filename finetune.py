bc534b010aeda18b3e6422a664b801d75c8d13de
Kirti0019 <122767331+Kirti0019@users.noreply.github.com>
2024-04-01 10:50:19 +0530

    Update finetune.py
    SFT_data

--- finetune.py
+++ finetune.py
@@ -28,7 +28,7 @@ from utils.prompter import Prompter
 def train(
     # model/data params
     base_model: str = "",  # the only required argument
-    data_path: str = "huggingface/datasets/sst2",
+    data_path: str = "SFT_data",
     output_dir: str = "./lora-alpaca",
     # training hyperparams
     batch_size: int = 128,
